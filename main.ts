e00c3aba04a6d26f00c41f015b50340d9f9b608e
shinylight <2530444+shinylightdev@users.noreply.github.com>
2020-11-02 17:05:40 -0500

    Testing

--- main.ts
+++ main.ts
@@ -7,15 +7,21 @@ import { ProblemSet } from 'models/ProblemSet';
 // console.log(y);
 
 
-// let p = new Problem(
-//           "+", 
-//           [
-//             new Operand(1, 5), 
-//             new Operand(4, 6),
-//             new Operand(500, 600),
-//             new Operand(1000, 2000)
-//           ]
-//         );
+let p = new Problem(
+          "+", 
+          [
+            new Operand(1, 5), 
+            new Operand(4, 6),
+            new Operand(500, 600),
+            new Operand(1000, 2000)
+          ]
+        );
+        
+        
+// console.log(p.generateExpression());
+// console.log(p.getAnswer());
+// console.log(p.getSubtraction());
+// console.log(p.getSum());
         
 
 // let p = new Problem(
@@ -42,5 +48,8 @@ import { ProblemSet } from 'models/ProblemSet';
 
 
 
-let x = new ProblemSet(3, "+");
+//let x = new ProblemSet(3, "+");
+
+
+
 
